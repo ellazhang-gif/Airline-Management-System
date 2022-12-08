f64b5e99c1faf4442e90798db15de8bbf05689e9
Shiyuan Liu <73524556+ShiyuanLiu111@users.noreply.github.com>
2022-12-09 00:57:24 +0800

    Add files via upload

---
+++ create_table_1209.sql
@@ -0,0 +1,120 @@
+create table Airline
+	(airline_name	varchar(20),
+	 primary key (airline_name)
+	);
+
+create table Airline_Staff
+    (staff_user_name      varchar(20),
+     staff_password       varchar(20) not null,
+     staff_first_name     varchar(20) not null,
+     staff_last_name      varchar(20) not null,
+     staff_date_of_birth  date not null,
+     airline_name         varchar(20),
+     primary key (staff_user_name),
+     foreign key (airline_name) references Airline(airline_name)
+        on delete set null
+    );
+
+create table Permission
+    (staff_user_name	varchar(20) not null,
+     permission_name    varchar(10) check (permission_name = 'Admin' or permission_name = 'Operator'),
+	 primary key (staff_user_name, permission_name),
+     foreign key (staff_user_name) references Airline_Staff(staff_user_name)
+        on delete cascade
+	);
+
+create table Airport
+    (airport_name	varchar(5),
+     airport_city   varchar(15) not null,
+	 primary key (airport_name)
+	);
+
+create table Airport_for_Airline
+    (airport_name	varchar(20) not null,
+     airline_name   varchar(20) not null,
+	 primary key (airport_name, airline_name),
+     foreign key (airport_name) references Airport(airport_name)
+        on delete cascade,
+     foreign key (airline_name) references Airline(airline_name)
+        on delete cascade
+	);
+
+create table Airplane
+    (airplane_id    varchar(10) not null,
+     airline_name   varchar(20) not null,
+     seats      int(11) not null,
+     primary key(airplane_id, airline_name),
+     foreign key (airline_name) references Airline(airline_name)
+        on delete cascade
+    );
+
+create table Flight
+    (airline_name   varchar(20) not null,
+     flight_num     varchar(10) not null,
+     departure_time datetime NOT NULL,
+     arrival_time   datetime NOT NULL,
+     price          numeric(6,0) not null,
+     status         varchar(20) not null,
+     arrival_airport_name   varchar(5),
+     departure_airport_name varchar(5),
+     airplane_id    varchar(10),
+     primary key (airline_name, flight_num),
+     foreign key (airline_name) references Airline(airline_name)
+        on delete cascade,
+     foreign key (arrival_airport_name) references Airport(airport_name)
+        on delete set null,
+     foreign key (departure_airport_name) references Airport(airport_name)
+        on delete set null,
+     foreign key (airplane_id) references Airplane(airplane_id)
+        on delete set null
+    );
+
+create table Booking_Agent
+    (booking_agent_email        varchar(20) not null,
+     booking_agent_password     varchar(20) not null,
+     booking_agent_id           varchar(20) not null,
+     primary key (booking_agent_email)
+    );
+
+create table Customer
+    (customer_email     varchar(20) not null,
+     customer_name      varchar(20) not null,
+     customer_password  varchar(20) not null,
+     building_number    varchar(7) not null,
+     street             varchar(30) not null,
+     city               varchar(15) not null,
+     state              varchar(15) not null,
+     phone_number       varchar(15) not null,
+     passport_number    varchar(15) not null,
+     passport_expiration    date not null,
+     passprt_country    varchar(15) not null,
+     customer_date_of_birth date not null,
+     primary key (customer_email)
+    );
+
+create table Ticket
+    (ticket_id      int(20),
+     airline_name   varchar(20),
+     flight_num     varchar(10),
+     customer_email varchar(20),
+     booking_agent_email varchar(20),
+     purchase_date date,
+     primary key (ticket_id),
+     foreign key (airline_name, flight_num) references Flight(airline_name, flight_num)
+        on delete set null,
+     foreign key (customer_email) references Customer(customer_email)
+        on delete set null,
+     foreign key (booking_agent_email) references Booking_Agent(booking_agent_email)
+        on delete set null
+    );
+
+create table Booking_Agent_Work_For
+    (booking_agent_email      varchar(20) not null,
+     airline_name             varchar(20) not null,
+     primary key (booking_agent_email, airline_name),
+     foreign key (airline_name) references Airline(airline_name)
+        on delete cascade,
+     foreign key (booking_agent_email) references Booking_Agent(booking_agent_email)
+        on delete cascade
+    );
+
---
+++ insert_value_1209.sql
@@ -0,0 +1,169 @@
+insert into Airline values ('China Eastern');
+insert into Airline values ('China International Airlines');
+insert into Airline values ('Delta Airlines');
+insert into Airline values ('American Airlines');
+insert into Airline values ('Korean Air');
+
+insert into Airport values ('JFK', 'New York City');
+insert into Airport values ('PVG', 'Shanghai');
+insert into Airport values ('PEK', 'Beijing');
+insert into Airport values ('BOS','Boston');
+insert into Airport values ('LAX', 'Los Angeles');
+insert into Airport values ('ICN', 'Incheon');
+insert into Airport values ('TAO', 'Qingdao');
+
+insert into Customer values ('shiyuan123@126.com', 'Shiyuan Liu', 'shiyuan888',
+    '1550', 'Century Avenue', 'Shanghai', 'Shanghai', '1501234567', 'EE673521', '2028-08-26',
+    'China', '2000-01-01');
+insert into Customer values ('xiaoming@163.com', 'Xiaoming Wang', 'xiaoming777',
+    '33', 'Apple Street', 'Beijing', 'Beijing', '18667549382', 'EE156382', '2025-03-14',
+    'China', '2002-12-10');
+insert into Customer values ('amy456@gmail.com', 'Amy Park', 'amygoodgoodstudy',
+    '14', '38th Street', 'New York', 'New York', '542375632', 'UT384762', '2022-12-06',
+    'US', '1992-08-26');
+insert into Customer values ('tom123@gmail.com', 'Tom Green', 'tomisagoodboy', 
+    '233', 'Bay State Road', 'Boston', 'Massachusetts', '577826234', 'UT52832','2031-10-10',
+    'US', '1989-12-21');
+insert into Customer values ('62836192@qq.com', 'Jinyuan Xi', 'jinyuan666',
+    '2', 'Fucheng Road', 'Beijing', 'Beijing', '58591587', 'EE01321', '2029-08-01',
+    'China', '1973-02-26');
+insert into Customer values ('andyzhang@126.com', 'Andy Zhang', 'hahahahaha',
+    '11', 'East Ocean Road', 'New York', 'New York', '526192731', 'UT761823', '2025-09-31',
+    'US', '1992-03-04');
+
+insert into Booking_Agent values ('citytrip@163.com', 'citytrip134', '034');
+insert into Booking_Agent values ('Tuniulvxing@qq.com', 'tuniu888', '145');
+insert into Booking_Agent values ('expedia@gmail.com', 'expedia66', '021');
+insert into Booking_Agent values ('taobao@126.com', 'taobaohaha', '093');
+insert into Booking_Agent values ('thankyoutrip@163.com', 'thankyousir', '372');
+insert into Booking_Agent values ('dontknowwhattoadd@qq.com', 'lmao66', '999');
+
+insert into Airplane values ('0935', 'China Eastern', '200');
+insert into Airplane values ('4726', 'China International Airline', '200');
+insert into Airplane values ('0567', 'Delta Airlines', '150');
+insert into Airplane values ('2112', 'China International Airline', '100');
+insert into Airplane values ('0098', 'Delta Airlines', '50');
+insert into Airplane values ('0542', 'China Eastern', '100');
+insert into Airplane values ('1473', 'Korean Air', '100');
+insert into Airplane values ('8712', 'American Airlines', '30');
+insert into Airplane values ('3333', 'American Airlines', '60');
+insert into Airplane values ('6666', 'Korean Air', '200');
+
+insert into Airport_for_Airline values('JFK', 'China Eastern');
+insert into Airport_for_Airline values('PVG', 'China Eastern');
+insert into Airport_for_Airline values('PEK', 'China Eastern');
+insert into Airport_for_Airline values('BOS', 'China Eastern');
+insert into Airport_for_Airline values('LAX', 'China Eastern');
+insert into Airport_for_Airline values('JFK', 'China International Airlines');
+insert into Airport_for_Airline values('PVG', 'China International Airlines');
+insert into Airport_for_Airline values('PEK', 'China International Airlines');
+insert into Airport_for_Airline values('BOS', 'China International Airlines');
+insert into Airport_for_Airline values('LAX', 'China International Airlines');
+insert into Airport_for_Airline values('JFK', 'Delta Airlines');
+insert into Airport_for_Airline values('PVG', 'Delta Airlines');
+insert into Airport_for_Airline values('PEK', 'Delta Airlines');
+insert into Airport_for_Airline values('BOS', 'Delta Airlines');
+insert into Airport_for_Airline values('LAX', 'Delta Airlines');
+insert into Airport_for_Airline values('JFK', 'American Airlines');
+insert into Airport_for_Airline values('PVG', 'American Airlines');
+insert into Airport_for_Airline values('PEK', 'American Airlines');
+insert into Airport_for_Airline values('BOS', 'American Airlines');
+insert into Airport_for_Airline values('LAX', 'American Airlines');
+insert into Airport_for_Airline values('JFK', 'Korean Air');
+insert into Airport_for_Airline values('PEK', 'Korean Air');
+insert into Airport_for_Airline values('ICN', 'Korean Air');
+
+
+insert into Airline_Staff values ('JZHANG564', 'qwerty625', 'Jiashuai', 'Zhang', '1980-09-03', 
+    'China Eastern');
+insert into Airline_Staff values ('Meili123', '456qrwycn', 'Meili', 'Liu', '1992-12-01',
+    'China International Airline');
+insert into Airline_Staff values ('wasai66', 'qwerty', 'Waisai', 'Zhang', '1999-08-09',
+    'China International Airline');
+insert into Airline_Staff values ('Jack456', '3772lalala', 'Jack', 'Ma', '1970-08-26',
+    'Delta Airlines');
+insert into Airline_Staff values ('Steven88', '99998888', 'Steven', 'James', '1993-04-05',
+    'American Airlines');
+insert into Airline_Staff values ('Kiko11', 'yyyyzzzz', 'Kiko', 'PG', '1989-12-12',
+    'Korean Air');
+
+insert into Permission values ('JZHANG564', 'Operator');
+insert into Permission values ('JZHANG564', 'Admin');
+insert into Permission values ('Meili123', 'Admin');
+insert into Permission values ('wasai66', 'Operator');
+insert into Permission values ('Jack456', 'Admin');
+insert into Permission values ('Jack456', 'Operator');
+insert into Permission values ('Steven88', 'Admin');
+insert into Permission values ('Steven88', 'Operator');
+insert into Permission values ('Kiko11', 'Admin');
+insert into Permission values ('Kiko11', 'Operator');
+
+
+insert into Flight values ('China Eastern', 'MU588', '2022-12-22 20:00:00', '2022-12-23 09:30:00',
+    '18700', 'upcoming', 'PVG', 'JFK', '0935');
+insert into Flight values ('China Eastern', 'MU3473', '2022-12-03 18:45:00', '2022-12-03 20:22:00',
+    '1021', 'delayed', 'PVG', 'PEK', '0542');
+insert into Flight values ('China Eastern', 'MU319', '2022-12-25 10:30:00', '2022-12-25 23:48:00',
+    '9857', 'upcoming', 'LAX', 'PEK', '0935');
+insert into Flight values ('China International Airline', 'CA277', '2022-06-05 14:30:00', '2022-06-06 03:10:00',
+    '16352', 'delayed', 'LAX', 'PVG', '4726');
+insert into Flight values ('China International Airline', 'CA5473', '2022-12-17 15:25:00', '2022-12-17 17:05:00',
+    '654', 'upcoming', 'PEK', 'PVG', '2112');
+insert into Flight values ('China International Airline', 'CA388', '2022-12-15 07:05:00', '2022-12-15 19:33:00',
+    '23100', 'in-progress', 'PEK', 'JFK', '4726');
+insert into Flight values ('Delta Airlines', 'DA522', '2022-12-04 22:35:00', '2022-12-05 09:40:00',
+    '9782', 'in-progess', 'BOS', 'PVG', '0567');
+insert into Flight values ('Delta Airlines', 'DA144', '2022-12-08 13:15:00', '2022-12-08 14:20:00',
+    '453', 'upcoming', 'BOS', 'JFK', '0098'); 
+insert into Flight values ('Korean Air', 'KE997', '2022-12-09 14:05:00', '2022-12-09 17:20:00',
+    '9930', 'upcoming', 'ICN', 'PEK', '1473');
+insert into Flight values ('Korean Air', 'KE054', '2022-12-02 18:30:00', '2022-12-03 06:17:00',
+    '29811', 'delayed', 'JFK', 'ICN', '6666');
+insert into Flight values ('American Airlines', 'AA876', '2022-12-07 16:07:00', '2022-12-09 21:35:00',
+    '1879', 'upcoming', 'LAX', 'BOS', '8712');
+insert into Flight values ('American Airlines', 'AA761', '2022-12-05 15:33:00', '2022-12-05 20:02:00',
+    '877', 'in-progress', 'BOS', 'LAX', '3333');
+
+
+insert into Ticket values ('1', 'China Eastern', 'MU588', 'tom123@gmail.com', null, '2022-11-30');
+insert into Ticket values ('2', 'China Eastern', 'MU588', 'Amy456@gmail.com', 'citytrip@163.com', '2022-10-10');
+insert into Ticket values ('3', 'China Eastern', 'MU3473', 'tom123@gmail.com', null, '2022-09-29');
+insert into Ticket values ('4', 'China Eastern', 'MU3473', 'Amy456@gmail.com', 'expedia@gmail.com', '2021-12-21');
+insert into Ticket values ('5', 'China Eastern', 'MU319', 'xiaoming@163.com', 'citytrip@163.com', '2022-05-04');
+insert into Ticket values ('6', 'Delta Airlines', 'DA144', 'xiaoming@163.com', 'expedia@gmail.com', '2022-05-15');
+insert into Ticket values ('7', 'China International Airline', 'CA277', 'Amy456@gmail.com', 'Tuniulvxing@qq.com', '2020-03-07');
+insert into Ticket values ('8', 'Delta Airlines', 'DA144', 'tom123@gmail.com', null, '2022-12-01');
+insert into Ticket values ('9', 'China Eastern', 'MU319', 'tom123@gmail.com', null, '2022-11-30');
+insert into Ticket values ('10', 'China International Airline', 'CA277', 'tom123@gmail.com', null, '2020-10-09');
+insert into Ticket values ('11', 'China International Airline', 'CA277', 'Amy456@gmail.com', 'expedia@gmail.com', '2021-01-03');
+insert into Ticket values ('12', 'China International Airline', 'CA5473', 'xiaoming@163.com', 'Tuniulvxing@qq.com', '2022-02-04');
+insert into Ticket values ('13', 'China International Airline', 'CA5473', 'tom123@gmail.com', null, '2021-04-07');
+insert into Ticket values ('14','China International Airline', 'CA388', 'xiaoming@163.com', 'expedia@gmail.com', '2022-01-28');
+insert into Ticket values ('15','China International Airline', 'CA388', 'Amy456@gmail.com', 'citytrip@163.com', '2022-03-01');
+insert into Ticket values ('16','Delta Airlines', 'DA522', 'tom123@gmail.com', null, '2022-04-05');
+insert into Ticket values ('17','Delta Airlines', 'DA522', 'Amy456@gmail.com', 'citytrip@163.com', '2021-11-08');
+insert into Ticket values ('18','American Airlines', 'AA876', 'shiyuan123@126.com', 'thankyoutrip@163.com', '2021-06-05');
+insert into Ticket values ('19','American Airlines', 'AA876', 'andyzhang@126.com', 'citytrip@163.com', '2022-10-03');
+insert into Ticket values ('20','American Airlines', 'AA761', 'shiyuan123@126.com', null, '2022-05-02');
+insert into Ticket values ('21','American Airlines', 'AA761', 'andyzhang@126.com', null, '2022-05-16');
+insert into Ticket values ('22','American Airlines', 'AA761', 'amy456@gmail.com', 'dontknowwhattoadd@qq.com', '2022-03-17');
+insert into Ticket values ('23','Korean Air', 'KE054', 'amy456@gmail.com', 'taobao@126.com', '2020-10-18');
+insert into Ticket values ('24','Korean Air', 'KE054', 'shiyuan123@126.com', 'dontknowwhattoadd@qq.com', '2021-06-09');
+insert into Ticket values ('25','Korean Air', 'KE054', '62836192@qq.com', null, '2022-04-07');
+insert into Ticket values ('26','Korean Air', 'KE997', '62836192@qq.com', null, '2022-09-10');
+insert into Ticket values ('27','Korean Air', 'KE997', 'xiaoming@163.com', 'taobao@126.com', '2022-01-17');
+
+insert into Booking_Agent_Work_For values('citytrip@163.com', 'China Eastern');
+insert into Booking_Agent_Work_For values('expedia@gmail.com', 'China Eastern');
+insert into Booking_Agent_Work_For values('citytrip@163.com', 'China International Airline');
+insert into Booking_Agent_Work_For values('Tuniulvxing@qq.com', 'China International Airline');
+insert into Booking_Agent_Work_For values('expedia@gmail.com', 'China International Airline');
+insert into Booking_Agent_Work_For values('citytrip@163.com', 'Delta Airlines');
+insert into Booking_Agent_Work_For values('expedia@gmail.com', 'Delta Airlines');
+insert into Booking_Agent_Work_For values('thankyoutrip@163.com', 'Delta Airlines');
+insert into Booking_Agent_Work_For values('dontknowwhattoadd@qq.com', 'Delta Airlines');
+insert into Booking_Agent_Work_For values('citytrip@163.com', 'American Airlines');
+insert into Booking_Agent_Work_For values('thankyoutrip@163.com', 'American Airlines');
+insert into Booking_Agent_Work_For values('dontknowwhattoadd@qq.com', 'American Airlines');
+insert into Booking_Agent_Work_For values('taobao@126.com', 'Korean Air');
+insert into Booking_Agent_Work_For values('dontknowwhattoadd@qq.com', 'Korean Air');
